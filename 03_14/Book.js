45bc0481b526fee155033c3f1d127c0d43147338
Deandre Coleman <76710242+ShockedAlloy@users.noreply.github.com>
2022-02-17 17:26:06 -0800

    Update Book.js

--- 03_14/Book.js
+++ 03_14/Book.js
@@ -1,1 +1,30 @@
+class Book {
+  constructor(
+    name,
+    author,
+    numOfPages,
+    format,
+    genre,
+    bookOpen,
+    pagesRead
+  ) {
+    this.name = name;
+    this.author = author;
+    this.numOfPages = numOfPages;
+    this.format = format;
+    this.genre = genre;
+    this.bookOpen = bookOpen;
+    this.pagesRead = pagesRead;
+  }
+  toggleBook(bookStatus) {
+    this.bookOpen = bookStatus;
+  }
+  currentPage() {
+    let pageStart = 0;
+    let pagesRead = new pagesRead;
+    let totalPages = pageStart - pagesRead; // elapsed time in milliseconds
+    return totalPages;
+  }
+}
 
+export default Book;
